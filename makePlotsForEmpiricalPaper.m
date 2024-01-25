744ec5051efcff993f8072bffa5353ea74c3890c
jCalderTravis <38797399+jCalderTravis@users.noreply.github.com>
2024-01-25 16:25:19 +0100

    Final tidy

--- makePlotsForEmpiricalPaper.m
+++ makePlotsForEmpiricalPaper.m
@@ -51,165 +51,165 @@ origNames = {   'NDscFlatNoneSameMvar',...
 
 
 %% Illustrations
-% dispMsg('Illustrations')
-% 
-% bayesianConfidenceIllustration('rawEv');
-% mT_exportNicePdf(15.9, 15.9, saveDir, 'confIllustration_rawEv')
-% 
-% bayesianConfidenceIllustration('pCorrect');
-% mT_exportNicePdf(15.9, 15.9, saveDir, 'confIllustration_pCorrect')
-% 
-% bayesianConfidenceIllustration('logPostRatioForChoice');
-% mT_exportNicePdf(15.9, 15.9, saveDir, 'confIllustration_logPostRatio')
-% 
-% bayesianConfidenceIllustration('logPostRatioForChoice2');
-% mT_exportNicePdf(15.9, 15.9, saveDir, 'confIllustration_logPostRatio2')
-% 
-% bayesianConfidenceIllustration('logPostRatioForChoice', 'full');
-% mT_exportNicePdf(15.9, 15.9, saveDir, ...
-%     'confIllustration_logPostRatio_fullRange')
-% 
-% bayesianConfidenceIllustration('logPostRatioForChoice2', 'full');
-% mT_exportNicePdf(15.9, 15.9, saveDir, ...
-%     'confIllustration_logPostRatio2_fullRange')
-% 
-% figure
-% x = -100:0.01:100;
-% y = normpdf(x, 0, 20);
-% plot(x, y)
-% mT_exportNicePdf(15.9, 15.9, saveDir, 'confIllustration_normalDist')
-% 
-% 
-% %% Basic data plots
-% dispMsg('Real data plots')
-% 
-% if strcmp(plotStat, 'mean')
-%     LoadedFiles = load(RealData);
-%     DSet = LoadedFiles.DSet;
-% 
-%     plotDencity(DSet)
-%     mT_exportNicePdf(15.9/2, 15.9, saveDir, 'expB_dencities')
-%     close all
-% end
-% 
-% LoadedFiles = load(RealData);
-% DSet = LoadedFiles.DSet;
-% DSet = prepDataWrapper(DSet, 4, true);
-% 
-% figHandle = figure;
-% for plotType = {'scatter', 'line'}
-%     figHandle = plotConfAgainstTimeAndEv(DSet, plotType{1}, figHandle, ...
-%         'binned', false, false, 'B', 'noEv', [], plotStat, [], true);
-% end
-% mT_exportNicePdf(15.9/2, 15.9/2, saveDir, 'confTimeMidpointInteraction')
-% close all
-% 
-% if strcmp(plotStat, 'mean')
-%     figHandle = figure;
-%     plotConfFrequencies(DSet, 'scatter', figHandle);
-% end
-% 
-% figHandle = figure;
-% figHandle = plotAccAndConfWithRt(DSet, 'scatter', 'B', false, ...
-%     figHandle, plotStat);
-% plotAccAndConfWithRt(DSet, 'line', 'B', false, ...
-%     figHandle, plotStat);
-% mT_exportNicePdf(15.9, 15.9/2, saveDir, 'expB_combinedTime')
-% close all
-% 
-% % Plots but looking at raw confidence instead of binned conf
-% f = figure;
-% plotConfAgainstTimeAndEv(DSet, 'scatter', f, 'raw', false, false, 'B')
-% mT_exportNicePdf(15.9, 15.9/2, saveDir, 'expB_rawConfEffects')
-% close all
-% 
-% 
-% %% Qualitative analysis
-% dispMsg('Quantiative analysis')
-% if strcmp(plotStat, 'mean')
-%     [medianNonDecis, evEffectFig] = runStatistics(DSet);
-%     figure(evEffectFig)
-%     mT_exportNicePdf(15.9/2, 15.9*(7/12), saveDir, 'expB_evEffectCoefs')
-%     close all
-% end
-% 
-% 
-% %% Evidence residuals
-% dispMsg('Evidence residuals')
-% EvDSet = convertToEvSet(DSet, true);
-% 
-% % Shade the region corresponding to non-decision time (Stim frame duration 
-% % was 50ms)
-% shadedRegion = [-medianNonDecis*0.05, 0];
-% 
-% plotFig = plotEvResidualsEffectWithTime(EvDSet, 'scatter', 'B', ...
-%     true, false, [], [], plotStat, shadedRegion);
-% plotFig = plotEvResidualsEffectWithTime(EvDSet, 'line', 'B', ...
-%     true, false, plotFig, [], plotStat);
-% figure(plotFig)
-% mT_exportNicePdf(15.9, 15.9, saveDir, 'expB_combinedEv')
-% 
-% 
-% %% Modelling results 
-% dispMsg('Modelling main overview')
-% 
-% if strcmp(plotStat, 'mean')
-%     [~, crossValPlot, CvDSets] = runCrossValEval(CrossValidationFits, false, ...
-%         true, modelNames, false, true);
-%     assert(isequal(origNames, mT_findAppliedModels(CvDSets{1})))
-% 
-%     figure(crossValPlot)
-%     mT_exportNicePdf(15.9/2, 15.9, saveDir, 'crossValComparison')
-% end
-% 
+dispMsg('Illustrations')
+
+bayesianConfidenceIllustration('rawEv');
+mT_exportNicePdf(15.9, 15.9, saveDir, 'confIllustration_rawEv')
+
+bayesianConfidenceIllustration('pCorrect');
+mT_exportNicePdf(15.9, 15.9, saveDir, 'confIllustration_pCorrect')
+
+bayesianConfidenceIllustration('logPostRatioForChoice');
+mT_exportNicePdf(15.9, 15.9, saveDir, 'confIllustration_logPostRatio')
+
+bayesianConfidenceIllustration('logPostRatioForChoice2');
+mT_exportNicePdf(15.9, 15.9, saveDir, 'confIllustration_logPostRatio2')
+
+bayesianConfidenceIllustration('logPostRatioForChoice', 'full');
+mT_exportNicePdf(15.9, 15.9, saveDir, ...
+    'confIllustration_logPostRatio_fullRange')
+
+bayesianConfidenceIllustration('logPostRatioForChoice2', 'full');
+mT_exportNicePdf(15.9, 15.9, saveDir, ...
+    'confIllustration_logPostRatio2_fullRange')
+
+figure
+x = -100:0.01:100;
+y = normpdf(x, 0, 20);
+plot(x, y)
+mT_exportNicePdf(15.9, 15.9, saveDir, 'confIllustration_normalDist')
+
+
+%% Basic data plots
+dispMsg('Real data plots')
+
+if strcmp(plotStat, 'mean')
+    LoadedFiles = load(RealData);
+    DSet = LoadedFiles.DSet;
+
+    plotDencity(DSet)
+    mT_exportNicePdf(15.9/2, 15.9, saveDir, 'expB_dencities')
+    close all
+end
+
+LoadedFiles = load(RealData);
+DSet = LoadedFiles.DSet;
+DSet = prepDataWrapper(DSet, 4, true);
+
+figHandle = figure;
+for plotType = {'scatter', 'line'}
+    figHandle = plotConfAgainstTimeAndEv(DSet, plotType{1}, figHandle, ...
+        'binned', false, false, 'B', 'noEv', [], plotStat, [], true);
+end
+mT_exportNicePdf(15.9/2, 15.9/2, saveDir, 'confTimeMidpointInteraction')
+close all
+
+if strcmp(plotStat, 'mean')
+    figHandle = figure;
+    plotConfFrequencies(DSet, 'scatter', figHandle);
+end
+
+figHandle = figure;
+figHandle = plotAccAndConfWithRt(DSet, 'scatter', 'B', false, ...
+    figHandle, plotStat);
+plotAccAndConfWithRt(DSet, 'line', 'B', false, ...
+    figHandle, plotStat);
+mT_exportNicePdf(15.9, 15.9/2, saveDir, 'expB_combinedTime')
+close all
+
+% Plots but looking at raw confidence instead of binned conf
+f = figure;
+plotConfAgainstTimeAndEv(DSet, 'scatter', f, 'raw', false, false, 'B')
+mT_exportNicePdf(15.9, 15.9/2, saveDir, 'expB_rawConfEffects')
+close all
+
+
+%% Qualitative analysis
+dispMsg('Quantiative analysis')
+if strcmp(plotStat, 'mean')
+    [medianNonDecis, evEffectFig] = runStatistics(DSet);
+    figure(evEffectFig)
+    mT_exportNicePdf(15.9/2, 15.9*(7/12), saveDir, 'expB_evEffectCoefs')
+    close all
+end
+
+
+%% Evidence residuals
+dispMsg('Evidence residuals')
+EvDSet = convertToEvSet(DSet, true);
+
+% Shade the region corresponding to non-decision time (Stim frame duration 
+% was 50ms)
+shadedRegion = [-medianNonDecis*0.05, 0];
+
+plotFig = plotEvResidualsEffectWithTime(EvDSet, 'scatter', 'B', ...
+    true, false, [], [], plotStat, shadedRegion);
+plotFig = plotEvResidualsEffectWithTime(EvDSet, 'line', 'B', ...
+    true, false, plotFig, [], plotStat);
+figure(plotFig)
+mT_exportNicePdf(15.9, 15.9, saveDir, 'expB_combinedEv')
+
+
+%% Modelling results 
+dispMsg('Modelling main overview')
+
+if strcmp(plotStat, 'mean')
+    [~, crossValPlot, CvDSets] = runCrossValEval(CrossValidationFits, false, ...
+        true, modelNames, false, true);
+    assert(isequal(origNames, mT_findAppliedModels(CvDSets{1})))
+
+    figure(crossValPlot)
+    mT_exportNicePdf(15.9/2, 15.9, saveDir, 'crossValComparison')
+end
+
 [DSet, FigureHandles] = mT_analyseClusterResults(FullDataSetFits, ...
     1, true, false, true, modelNames); 
 DSet = DSet{1};
 assert(isequal(origNames, mT_findAppliedModels(DSet)))
 
-% [~, bicData] = mT_collectBicAndAicInfo(DSet);
-% runBicBasedMixEffectsModelCompare(bicData, modelNames)
-% 
-% if strcmp(plotStat, 'mean')
-%     figure(FigureHandles.AicBic)
-%     mT_exportNicePdf(15.9, 15.9, saveDir, 'aicBic')
-% 
-%     % The threshold parameters are in a random order. Sort to be in
-%     % order before making the parameter table for latex
-%     TmpDSet = DSet;
-%     for iP = 1 : length(TmpDSet.P)
-%         for iM = 1 : length(TmpDSet.P(iP).Models)
-%             oldThresh = TmpDSet.P(iP).Models(iM).BestFit.Params.Thresholds;
-%             assert(size(oldThresh, 2) == 1)
-%             TmpDSet.P(iP).Models(iM).BestFit.Params.Thresholds = ...
-%                 sort(oldThresh);
-%         end
-%     end
-% 
-%     ParamLabels.Sigma_phi = "$\sigma_\varphi$";
-%     ParamLabels.Sigma_acc = "$\sigma_{acc}$";
-%     ParamLabels.Thresholds = "$d_i$";
-%     ParamLabels.BoundIntercept = "$a$";
-%     ParamLabels.PipelineI = "$I$ (s)";
-%     ParamLabels.LapseRate = "$\lambda$";
-%     ParamLabels.MetacogNoise = "$\sigma_m$";
-%     ParamLabels.NoiseRatio = "$\Gamma$";
-%     mT_produceParamStats(TmpDSet, saveDir, [2, 5, 7], ParamLabels, ...
-%                             [], modelNames)
-%     close all
-% 
-%     % Fit end points
-%     [~, restartsFigure] = mT_plotFitEndPoints(DSet, false, 1, [], modelNames);
-%     figure(restartsFigure)
-%     mT_exportNicePdf(15.9/2, 15.9*(2/3), saveDir, 'restartsRequired')
-% end
-% 
-% %% Main modelling fits
-% dispMsg('Modelling main fits')
-% 
-% plotAndSaveConfidencePredictions(DSet, 7, plotStat, saveDir, '')
-% 
-% makeCombinedPoorModelsPlot(DSet, [2, 5], modelNames, plotStat, saveDir)
+[~, bicData] = mT_collectBicAndAicInfo(DSet);
+runBicBasedMixEffectsModelCompare(bicData, modelNames)
+
+if strcmp(plotStat, 'mean')
+    figure(FigureHandles.AicBic)
+    mT_exportNicePdf(15.9, 15.9, saveDir, 'aicBic')
+
+    % The threshold parameters are in a random order. Sort to be in
+    % order before making the parameter table for latex
+    TmpDSet = DSet;
+    for iP = 1 : length(TmpDSet.P)
+        for iM = 1 : length(TmpDSet.P(iP).Models)
+            oldThresh = TmpDSet.P(iP).Models(iM).BestFit.Params.Thresholds;
+            assert(size(oldThresh, 2) == 1)
+            TmpDSet.P(iP).Models(iM).BestFit.Params.Thresholds = ...
+                sort(oldThresh);
+        end
+    end
+
+    ParamLabels.Sigma_phi = "$\sigma_\varphi$";
+    ParamLabels.Sigma_acc = "$\sigma_{acc}$";
+    ParamLabels.Thresholds = "$d_i$";
+    ParamLabels.BoundIntercept = "$a$";
+    ParamLabels.PipelineI = "$I$ (s)";
+    ParamLabels.LapseRate = "$\lambda$";
+    ParamLabels.MetacogNoise = "$\sigma_m$";
+    ParamLabels.NoiseRatio = "$\Gamma$";
+    mT_produceParamStats(TmpDSet, saveDir, [2, 5, 7], ParamLabels, ...
+                            [], modelNames)
+    close all
+
+    % Fit end points
+    [~, restartsFigure] = mT_plotFitEndPoints(DSet, false, 1, [], modelNames);
+    figure(restartsFigure)
+    mT_exportNicePdf(15.9/2, 15.9*(2/3), saveDir, 'restartsRequired')
+end
+
+%% Main modelling fits
+dispMsg('Modelling main fits')
+
+plotAndSaveConfidencePredictions(DSet, 7, plotStat, saveDir, '')
+
+makeCombinedPoorModelsPlot(DSet, [2, 5], modelNames, plotStat, saveDir)
 
 dispMsg('Modelling main full sim')
 plotFullSimFits(saveDir, DSet, 7, plotStat, '', 640*10, [], 'skipRtPlots')
@@ -236,8 +236,7 @@ plotFullSimFits(saveDir, RegDSet, iM, plotStat, ...
 %% Regularised CV fits
 
 if strcmp(plotStat, 'mean')
-    [~, crossValRegPlot, ~] = runCrossValEval(RegularisedCvFits, false, ...
-        true, [], true);
+    runCrossValEval(RegularisedCvFits, false, true, [], true);
 end
 
 
